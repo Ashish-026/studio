b0890caa6be06af9fd9388613bd117faafe78073
ASHISH KUMAR PRADHAN <ashishmahalaxmi.026@gmail.com>
2025-11-07 02:47:10 +0000

    No summary column should be there

--- src/components/private/private-dashboard.tsx
+++ src/components/private/private-dashboard.tsx
@@ -1,13 +1,11 @@
 'use client';
 
 import { PrivatePurchases } from './private-purchases';
-import { PrivateSummaryCards } from './private-summary-cards';
 
 
 export function PrivateDashboard() {
   return (
     <div className="space-y-8">
-      <PrivateSummaryCards />
       <PrivatePurchases />
     </div>
   );
