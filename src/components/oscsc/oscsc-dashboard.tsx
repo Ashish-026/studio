19a97a1bf9d5551abaf3817417a177a62a2bcef4
ASHISH KUMAR PRADHAN <ashishmahalaxmi.026@gmail.com>
2025-11-07 01:59:47 +0000

    In oscsc register there should be another display mentioning target allo

--- src/components/oscsc/oscsc-dashboard.tsx
+++ src/components/oscsc/oscsc-dashboard.tsx
@@ -4,11 +4,15 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { SummaryCards } from './summary-cards';
 import { TargetAllotment } from './target-allotment';
 import { PaddyLifted } from './paddy-lifted';
+import { MandiSummary } from './mandi-summary';
+import { Separator } from '../ui/separator';
 
 export function OscscDashboard() {
   return (
     <div className="space-y-8">
       <SummaryCards />
+      <MandiSummary />
+      <Separator />
       <Tabs defaultValue="paddy-lifted" className="space-y-4">
         <TabsList>
           <TabsTrigger value="target-allotment">Target Allotment</TabsTrigger>
