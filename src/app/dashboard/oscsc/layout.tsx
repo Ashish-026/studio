d32211273d0fef0a9dba536f8fea14ebef53eded
ASHISH KUMAR PRADHAN <ashishmahalaxmi.026@gmail.com>
2025-11-09 03:08:19 +0000

    We will shift to firebase after once completed that will solve few probl

--- src/app/dashboard/oscsc/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { OSCSCProvider } from '@/context/oscsc-context';
-
-export default function OscscLayout({ children }: { children: React.ReactNode }) {
-  return <OSCSCProvider>{children}</OSCSCProvider>;
-}
--- src/app/dashboard/private/layout.tsx
+++ src/app/dashboard/private/layout.tsx
@@ -1,5 +1,4 @@
-import { PrivateProvider } from '@/context/private-context';
 
 export default function PrivateLayout({ children }: { children: React.ReactNode }) {
-  return <PrivateProvider>{children}</PrivateProvider>;
+  return <>{children}</>;
 }
